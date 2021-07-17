c36c5303d49d604188b3b78b6561ec5c0c87694d
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-07-17 22:52:35 +0100

    Update fractal.py

--- fractal.py
+++ fractal.py
@@ -1,11 +1,26 @@
-def recursive_function(value,angle,branching_factor:int):
-    if stopping_condition(value,angle):
-        raise StopIteration
-        
-    do_something(value,angle)
+class Recursive:
+    def __init__(self, function:callable) -> None:
+        self.function = function
+        self.cache = {}
+        self.branching_factor = 3
+    
+    def memoise(self, *args):
+        key = '_'.join(str(arg) for arg in args)
+        if key not in self.cache:
+            self.cache[key] = self.function(*args)
+        return self.cache[key]
+    
+    def __call__(self, *args):
+        for _ in range(self.branching_factor):
+            return self.memoise(*args)
 
-    for _ in range(branching_factor):
-        recursive_function(*update(value,angle,branching_factor))
+    # if stopping_condition(value,angle):
+    #     raise StopIteration
+        
+    # do_something(value,angle)
+
+    # for _ in range(branching_factor):
+    #     recursive_function(*update(value,angle,branching_factor))
 
 
 from turtle import Screen, Turtle
@@ -13,24 +28,33 @@ from turtle import Screen, Turtle
 environment = Screen()
 agent = Turtle()
 
-seed_value = 50
-seed_angle = 180
-seed_branching_factor = 2
-
-def stopping_condition(value,angle):
-    return value <= 1
-
-def do_something(value,angle):
+@Recursive
+def flower(distance:int, angle:int):
+    agent.forward(distance)
     agent.right(angle)
-    agent.forward(value)
+    return flower(distance-1, angle+1)
 
-def update(value, angle, branching_factor):
-    new_value = value
-    new_angle = (angle + 89)%360
-    new_branching_factor = branching_factor
-    return new_value, new_angle, new_branching_factor
+flower(40,10)
+environment.exitonclick()
 
-try:
-    recursive_function(seed_value,seed_angle,seed_branching_factor)
-except StopIteration:
-    pass 
+# seed_value = 50
+# seed_angle = 180
+# seed_branching_factor = 2
+
+# def stopping_condition(value,angle):
+#     return value <= 1
+
+# def do_something(value,angle):
+#     agent.right(angle)
+#     agent.forward(value)
+
+# def update(value, angle, branching_factor):
+#     new_value = value
+#     new_angle = (angle + 89)%360
+#     new_branching_factor = branching_factor
+#     return new_value, new_angle, new_branching_factor
+
+# try:
+#     recursive_function(seed_value,seed_angle,seed_branching_factor)
+# except StopIteration:
+#     pass 
